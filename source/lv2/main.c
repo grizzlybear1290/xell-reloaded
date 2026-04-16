c36ea19f4e350324f1993f713b8d88bef6cead45
grizzlybear1290 <brewooddanny@gmail.com>
2026-04-16 09:41:39 +0100

    Update main.c

--- source/lv2/main.c
+++ source/lv2/main.c
@@ -112,13 +112,13 @@ int main(){
     dumpana();
 
 #ifdef SWIZZY_THEME
-    console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_ORANGE); // Orange text on black bg
+    console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_ORANGE); // purple text on white bg
 #elif defined XTUDO_THEME
-    console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_PINK); // Pink text on black bg
+    console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_PINK); // purple text on white bg
 #elif defined DEFAULT_THEME
-    console_set_colors(CONSOLE_COLOR_BLUE,CONSOLE_COLOR_WHITE); // White text on blue bg
-#else
-    console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_GREEN); // Green text on black bg
+    console_set_colors(CONSOLE_COLOR_BLUE,CONSOLE_COLOR_WHITE); // purple text on white bg
+#e
+    console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_GREEN); // purple text on white bg
 #endif
     console_init();
 
